938f5fe9362c1cb83f373725f25783f0252c7753
actonbp <40258526+actonbp@users.noreply.github.com>
2021-07-30 06:56:58 -0400

    inital from R studio

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# acton_website_2021
-Updated personal website thanks to ap hill's awesome theme!
+# hugo-apero
---
+++ config_page.R
@@ -0,0 +1,32 @@
+library(blogdown)
+
+#blogdown::install_hugo()
+
+#blogdown::new_site(theme = "apreshill/apero")
+
+blogdown::build_site(local=TRUE)
+
+###
+
+blogdown::serve_site()
+
+blogdown::stop_server()
+
+#blogdown::new_post("Tidy_Tuesday_2.21")
+
+setwd("C:/Users/bacto/OneDrive/Desktop/Programming/R/personal_site")
+
+#blogdown::new_post("test")
+
+####################
+.libPaths("C:/Users/bacto/R/library")
+#the above is a test
+
+
+
+
+####INFO on PREVIEW:
+#https://yihui.org/en/2017/08/hugo-post-summary/
+
+############
+
