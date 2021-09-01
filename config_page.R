92bfa10fc2f587186671b5988c08698a405b6920
acton_bp <actonbp@gmail.com>
2021-09-01 14:25:02 -0400

    utils

--- config_page.R
+++ config_page.R
@@ -19,16 +19,16 @@ setwd("C:/Users/bacto/OneDrive/Desktop/Programming/R/acton_website_updated_2")
 #blogdown::new_post("test")
 
 ####################
-.libPaths("C:/Users/bacto/R/library")
+#.libPaths("C:/Users/bacto/R/library")
 #the above is a test
 
 usethis::create_github_token() 
 
-credentials::set_github_pat()
+#credentials::set_github_pat()
 
 usethis::git_sitrep()
 ####INFO on PREVIEW:
 #https://yihui.org/en/2017/08/hugo-post-summary/
 
 ############
-
+blogdown::check_content()
